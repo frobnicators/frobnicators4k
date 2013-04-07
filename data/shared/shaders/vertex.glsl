13a7406c1814174d807c297a13dc442666ac8c4b
Andreas Tarandi <torandi@gmail.com>
2013-04-07 17:26:11 +0200

    Update vertex shader (only take vec2)

--- data/shared/shaders/vertex.glsl
+++ data/shared/shaders/vertex.glsl
@@ -1,9 +1,9 @@
-layout (location=0) in vec4 in_pos;
+layout (location=0) in vec2 in_pos;
 layout (location=1) in vec2 in_uv;
 
 out vec2 uv;
 
 void main(){
 	uv = in_uv;
-	gl_Position = matrix *  in_pos;
+	gl_Position = matrix *  vec4(in_pos, 0.0, 1.0);
 }
