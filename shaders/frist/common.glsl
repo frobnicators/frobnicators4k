228ed9e8f3d5e9d5fdb2d8e5c9fd1422e25d1f72
Andreas Tarandi <torandi@gmail.com>
2013-04-09 22:46:29 +0200

    "Celshading"

--- shaders/frist/common.glsl
+++ shaders/frist/common.glsl
@@ -32,15 +32,40 @@ vec3 n_at(vec3 p){ /* Calculate normal*/
 	return normalize(n);
 }
 
-vec3 light(vec3 pos, vec3 color) { /* calculate light */
+float brdr(vec3 pos, vec3 dir, vec3 normal, float d) {
+	float border = 0;
+	vec3 check_dir = cross(dir, normal);
+	vec3 color;
+	float weight = 1.0;
+	for(int i=0; i<5; ++i) {
+		float delta = pow( float (i + 1.0 ), 2.0 ) * 0.07;
+		border += abs(scene(pos + check_dir * delta, color) - d) * weight;
+		border += abs(scene(pos + check_dir * -delta, color) - d) * weight;
+		weight *= 0.5;
+	}
+	return step(0.1,border);
+}
+
+vec3 light(vec3 pos, vec3 dir, float d, vec3 color) { /* calculate light */
 	vec3 light_dir = pos - lpos();
 	float dist = length(light_dir);
 	light_dir = normalize(light_dir);
 
 	vec3 normal = n_at(pos);
 	float lambert_term = max(dot(-light_dir, normal), 0.0);
+
+	const int i_num_light_steps = 10;
+	const float i_step_size = 1.0/i_num_light_steps;
+
+	for(int i=0;i<i_num_light_steps;++i) {
+		if(lambert_term < i_step_size * (i+1)) {
+			lambert_term = i_step_size*i;
+			break;
+		}
+	}
 	vec3 ocolor = color * lambert_term * i_light_color;
-	return ocolor + color*i_ambient_light;
+	float border = brdr(pos, dir, normal, d);
+	return mix(ocolor + color*i_ambient_light, vec3(0), border);
 }
 
 vec3 raymarch(vec3 p, vec2 uv){
@@ -55,7 +80,7 @@ vec3 raymarch(vec3 p, vec2 uv){
 
 	for (int i = 0; i < i_max_steps && dist < i_max_dist; i++ ) {
 		float d = scene(p, color);
-		if ( d < i_epsilon ) return light(p, color);
+		if ( d < i_epsilon ) return light(p, dir, d, color);
 		dist += abs(d);
 		p += dir * d;
 	}
