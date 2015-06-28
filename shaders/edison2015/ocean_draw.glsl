5c0b0b0e41e58e93b29f486668ce09e8230455fd
David Sveningsson <ext@sidvind.com>
2015-06-29 00:05:28 +0300

    clouds

--- shaders/edison2015/ocean_draw.glsl
+++ shaders/edison2015/ocean_draw.glsl
@@ -51,50 +51,17 @@ bool terrainmarch(vec3 ro, vec3 rd, out vec3 hit, out float h){
 	return false;
 }
 
-vec3 permute(vec3 x){
-	return mod(((x*34.0) + 1.0)*x, 289);
-}
+vec3 sky_color(vec3 ro, vec3 rd){
+	const vec3 sky   = vec3(0.32, 0.58, 0.74);
+	const vec3 cloud = vec3(1.0, 0.95, 1.0);
+	const vec2 sc = ro.xz + rd.xz*(1000.0 - ro.y) / rd.y;
 
-float snoise(vec2 v){
-	const vec4 C = vec4(0.211, 0.366, -0.577, 0.024);
-	// First corner
-	vec2 i = floor(v + dot(v, C.yy));
-	vec2 x0 = v - i + dot(i, C.xx);
-
-	// Other corners
-	vec2 i1;
-	i1 = (x0.x > x0.y) ? vec2(1.0, 0.0) : vec2(0.0, 1.0);
-	vec4 x12 = x0.xyxy + C.xxzz;
-	x12.xy -= i1;
-
-	vec3 p = permute(permute(i.y + vec3(0.0, i1.y, 1.0))
-		+ i.x + vec3(0.0, i1.x, 1.0));
-
-	vec3 m = max(0.5 - vec3(dot(x0, x0), dot(x12.xy, x12.xy), dot(x12.zw, x12.zw)), 0.0);
-	m = pow(m, vec3(4));
-
-	vec3 x = 2.0 * fract(p * C.www) - 1.0;
-	vec3 h = abs(x) - 0.5;
-	vec3 ox = floor(x + 0.5);
-	vec3 a0 = x - ox;
-	m *= 1.79 - 0.85 * (a0*a0 + h*h);
-	vec3 g;
-	g.x = a0.x  * x0.x + h.x  * x0.y;
-	g.yz = a0.yz * x12.xz + h.yz * x12.yw;
-	return 130.0 * dot(m, g);
-}
-
-vec3 sky_color(vec3 rd){
-	const float theta = acos(rd.z / length(rd));
-	const float phi = atan(rd.y, rd.x);
-	const vec2 q = vec2(theta / 3.1415, phi / 3.1415 * 0.5 + 0.5);
-	return vec3(q, 0);
-	//return mix(vec3(0.8, 0.8, 1.0), vec3(0.9, 0.9, 0.9), clamp(snoise(rd.yz * 5), 0, 1));
+	vec3 color = mix(sky * (1.0 - rd.y), cloud, 0.5*smoothstep(0.1f, 0.7f, cnoise(sc*0.001)));
+	return mix(color, sky, pow(1.0 - max(rd.y, 0.0), 8.0));
 }
 
 vec3 ocean_color(vec3 ro, vec3 rd, vec3 hit, float h){
 	return (texture2D(texture0, hit.xz * 0.001).rgb + 1.f) * 0.5;
-	//return (h > 0 ? vec3(0, 0, 1) : vec3(-0.5, -0.5, -0.5)) * h;
 }
 
 void main() {
@@ -108,7 +75,7 @@ void main() {
 	if (terrainmarch(ro, rd, hit, h)){
 		oc = ocean_color(ro, rd, hit, h);
 	} else {
-		oc = sky_color(rd);
+		oc = sky_color(ro, rd);
 	}
 
 	/*
